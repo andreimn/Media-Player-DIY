94cc48ede29b0c6b726feba5617f7130ed3856e8
andreimn <andreimnbudchenko@gmail.com>
2022-06-10 21:30:01 +0300

    Добавьте файлы проекта.

---
+++ Form1.Designer.cs
@@ -0,0 +1,245 @@
+﻿namespace Media_Player_DIY
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.toolStrip1 = new System.Windows.Forms.ToolStrip();
+            this.toolStripButton1 = new System.Windows.Forms.ToolStripButton();
+            this.TimelineTrackbar = new System.Windows.Forms.TrackBar();
+            this.TrackBarVolume = new System.Windows.Forms.TrackBar();
+            this.tableLayoutPanel1 = new System.Windows.Forms.TableLayoutPanel();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button1 = new System.Windows.Forms.Button();
+            this.RepeatButton = new System.Windows.Forms.Button();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.NameTruckLabel = new System.Windows.Forms.Label();
+            this.toolStrip1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.TimelineTrackbar)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.TrackBarVolume)).BeginInit();
+            this.tableLayoutPanel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
+            // 
+            // toolStrip1
+            // 
+            this.toolStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.toolStripButton1});
+            this.toolStrip1.Location = new System.Drawing.Point(0, 0);
+            this.toolStrip1.Name = "toolStrip1";
+            this.toolStrip1.Size = new System.Drawing.Size(1148, 25);
+            this.toolStrip1.TabIndex = 1;
+            this.toolStrip1.Text = "toolStrip1";
+            // 
+            // toolStripButton1
+            // 
+            this.toolStripButton1.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Text;
+            this.toolStripButton1.Image = ((System.Drawing.Image)(resources.GetObject("toolStripButton1.Image")));
+            this.toolStripButton1.ImageTransparentColor = System.Drawing.Color.Magenta;
+            this.toolStripButton1.Name = "toolStripButton1";
+            this.toolStripButton1.Size = new System.Drawing.Size(90, 22);
+            this.toolStripButton1.Text = "Открыть файл";
+            this.toolStripButton1.Click += new System.EventHandler(this.toolStripButton1_Click);
+            // 
+            // TimelineTrackbar
+            // 
+            this.tableLayoutPanel1.SetColumnSpan(this.TimelineTrackbar, 4);
+            this.TimelineTrackbar.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.TimelineTrackbar.Enabled = false;
+            this.TimelineTrackbar.Location = new System.Drawing.Point(3, 540);
+            this.TimelineTrackbar.Maximum = 100;
+            this.TimelineTrackbar.Name = "TimelineTrackbar";
+            this.TimelineTrackbar.Size = new System.Drawing.Size(1082, 15);
+            this.TimelineTrackbar.TabIndex = 5;
+            this.TimelineTrackbar.TickStyle = System.Windows.Forms.TickStyle.None;
+            this.TimelineTrackbar.ValueChanged += new System.EventHandler(this.TimelineTrackbar_ValueChanged);
+            // 
+            // TrackBarVolume
+            // 
+            this.TrackBarVolume.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.TrackBarVolume.Location = new System.Drawing.Point(1091, 3);
+            this.TrackBarVolume.Maximum = 100;
+            this.TrackBarVolume.Name = "TrackBarVolume";
+            this.TrackBarVolume.Orientation = System.Windows.Forms.Orientation.Vertical;
+            this.tableLayoutPanel1.SetRowSpan(this.TrackBarVolume, 3);
+            this.TrackBarVolume.Size = new System.Drawing.Size(54, 531);
+            this.TrackBarVolume.TabIndex = 6;
+            this.TrackBarVolume.TickStyle = System.Windows.Forms.TickStyle.TopLeft;
+            this.TrackBarVolume.ValueChanged += new System.EventHandler(this.TrackBarVolume_ValueChanged);
+            // 
+            // tableLayoutPanel1
+            // 
+            this.tableLayoutPanel1.ColumnCount = 5;
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 25.00061F));
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 25.00063F));
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 25.00063F));
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 24.99813F));
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Absolute, 57F));
+            this.tableLayoutPanel1.Controls.Add(this.button2, 0, 1);
+            this.tableLayoutPanel1.Controls.Add(this.button3, 2, 1);
+            this.tableLayoutPanel1.Controls.Add(this.button1, 1, 1);
+            this.tableLayoutPanel1.Controls.Add(this.TrackBarVolume, 4, 0);
+            this.tableLayoutPanel1.Controls.Add(this.RepeatButton, 3, 1);
+            this.tableLayoutPanel1.Controls.Add(this.listBox1, 2, 0);
+            this.tableLayoutPanel1.Controls.Add(this.label1, 0, 0);
+            this.tableLayoutPanel1.Controls.Add(this.TimelineTrackbar, 0, 3);
+            this.tableLayoutPanel1.Controls.Add(this.NameTruckLabel, 0, 2);
+            this.tableLayoutPanel1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.tableLayoutPanel1.Location = new System.Drawing.Point(0, 25);
+            this.tableLayoutPanel1.Name = "tableLayoutPanel1";
+            this.tableLayoutPanel1.RowCount = 4;
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 86.54354F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 13.45646F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 45F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 20F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 20F));
+            this.tableLayoutPanel1.Size = new System.Drawing.Size(1148, 558);
+            this.tableLayoutPanel1.TabIndex = 7;
+            // 
+            // button2
+            // 
+            this.button2.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.button2.Image = global::Media_Player_DIY.Properties.Resources.BStart;
+            this.button2.Location = new System.Drawing.Point(3, 429);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(266, 60);
+            this.button2.TabIndex = 3;
+            this.button2.UseVisualStyleBackColor = true;
+            // 
+            // button3
+            // 
+            this.button3.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.button3.Image = global::Media_Player_DIY.Properties.Resources.BEnd;
+            this.button3.Location = new System.Drawing.Point(547, 429);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(266, 60);
+            this.button3.TabIndex = 4;
+            this.button3.UseVisualStyleBackColor = true;
+            // 
+            // button1
+            // 
+            this.button1.BackgroundImageLayout = System.Windows.Forms.ImageLayout.None;
+            this.button1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.button1.Image = global::Media_Player_DIY.Properties.Resources.BStop;
+            this.button1.Location = new System.Drawing.Point(275, 429);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(266, 60);
+            this.button1.TabIndex = 2;
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // RepeatButton
+            // 
+            this.RepeatButton.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.RepeatButton.Image = global::Media_Player_DIY.Properties.Resources.BRepeat_off;
+            this.RepeatButton.Location = new System.Drawing.Point(819, 429);
+            this.RepeatButton.Name = "RepeatButton";
+            this.RepeatButton.Size = new System.Drawing.Size(266, 60);
+            this.RepeatButton.TabIndex = 7;
+            this.RepeatButton.UseVisualStyleBackColor = true;
+            this.RepeatButton.Click += new System.EventHandler(this.RepeatButton_Click);
+            // 
+            // listBox1
+            // 
+            this.listBox1.AllowDrop = true;
+            this.tableLayoutPanel1.SetColumnSpan(this.listBox1, 2);
+            this.listBox1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.Location = new System.Drawing.Point(547, 3);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(538, 420);
+            this.listBox1.TabIndex = 8;
+            this.listBox1.SelectedIndexChanged += new System.EventHandler(this.listBox1_SelectedIndexChanged_1);
+            this.listBox1.DragDrop += new System.Windows.Forms.DragEventHandler(this.listBox1_DragDrop);
+            this.listBox1.DragEnter += new System.Windows.Forms.DragEventHandler(this.listBox1_DragEnter);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(3, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(35, 13);
+            this.label1.TabIndex = 9;
+            this.label1.Text = "label1";
+            // 
+            // NameTruckLabel
+            // 
+            this.NameTruckLabel.AutoSize = true;
+            this.tableLayoutPanel1.SetColumnSpan(this.NameTruckLabel, 4);
+            this.NameTruckLabel.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.NameTruckLabel.Location = new System.Drawing.Point(3, 492);
+            this.NameTruckLabel.Name = "NameTruckLabel";
+            this.NameTruckLabel.Size = new System.Drawing.Size(1082, 45);
+            this.NameTruckLabel.TabIndex = 10;
+            this.NameTruckLabel.Text = "Аудио файл не выбран";
+            this.NameTruckLabel.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1148, 583);
+            this.Controls.Add(this.tableLayoutPanel1);
+            this.Controls.Add(this.toolStrip1);
+            this.Name = "Form1";
+            this.Text = "MPMn";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.toolStrip1.ResumeLayout(false);
+            this.toolStrip1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.TimelineTrackbar)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.TrackBarVolume)).EndInit();
+            this.tableLayoutPanel1.ResumeLayout(false);
+            this.tableLayoutPanel1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+        private System.Windows.Forms.ToolStrip toolStrip1;
+        private System.Windows.Forms.ToolStripButton toolStripButton1;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.TrackBar TimelineTrackbar;
+        private System.Windows.Forms.TableLayoutPanel tableLayoutPanel1;
+        private System.Windows.Forms.TrackBar TrackBarVolume;
+        private System.Windows.Forms.Button RepeatButton;
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label NameTruckLabel;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,200 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Windows.Media;
+
+namespace Media_Player_DIY
+{
+    public partial class Form1 : Form
+    {
+        MediaPlayer mpUser = new MediaPlayer();
+        bool isMPplay = false;
+        bool isMPopened = false;
+        int isRepeat = 0;
+        TimeSpan TrackPositionTime;
+        Double TimeSound_double;
+        List<string> paths = new List<string>();
+
+        public Form1()
+        {
+            InitializeComponent();
+            openFileDialog1.FileName = "";
+
+            mpUser.MediaOpened += OpenedSound;
+            mpUser.MediaEnded += NextTrackPlay;
+
+
+        }
+
+        private void toolStripButton1_Click(object sender, EventArgs e)
+        {
+            openFileDialog1.Title = "Выберите файл";
+            //openFileDialog1.Filter = "Аудио файл | *.wav";
+            openFileDialog1.ShowDialog();
+            mpUser.Open(new Uri(openFileDialog1.FileName));
+            isMPopened = true;
+            TimeSound_double = (double)mpUser.NaturalDuration.TimeSpan.TotalSeconds;
+            label1.Text += TimeSound_double.ToString();
+
+
+
+        }
+
+        private void TimelineTrackbar_ValueChanged(object sender, EventArgs e)
+        {
+            double lasttime = TimeSound_double;
+            double SeekIndex = TimelineTrackbar.Value / 100f;
+
+            if (TimeSound_double > 60 && TimeSound_double < 3600)
+            {
+                int minuts = (int)Math.Floor(lasttime * SeekIndex / 60); lasttime -= minuts * 60;
+                int seconds = (int)Math.Floor(lasttime * SeekIndex) ;
+                TrackPositionTime = new TimeSpan(0, minuts, seconds);
+            }
+            else if(TimeSound_double > 3600 && TimeSound_double < 86400)
+            {
+                int hours = (int)Math.Floor(lasttime * SeekIndex / 3600); lasttime -= hours*3600;
+                int minuts = (int)Math.Floor(lasttime * SeekIndex / 60); lasttime -= minuts * 60;
+                int seconds = (int)Math.Floor(lasttime * SeekIndex);
+                TrackPositionTime = new TimeSpan(hours, minuts, seconds);
+            }
+
+            mpUser.Position = TrackPositionTime;
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (isMPopened && isMPplay == false)
+            {
+                isMPplay = true;
+                mpUser.Play();
+            }
+            else if (isMPopened && isMPplay == true)
+            {
+                isMPplay = false;
+                mpUser.Pause();  
+            }
+        }
+
+        private void TrackBarVolume_ValueChanged(object sender, EventArgs e)
+        {
+            mpUser.Volume = TrackBarVolume.Value / 100.0f;
+        }
+
+        private void RepeatButton_Click(object sender, EventArgs e)
+        {
+            if (isRepeat == 0)
+            {
+                isRepeat = 1;
+                RepeatButton.Image = Media_Player_DIY.Properties.Resources.BRepeat_once;
+            }
+            else if (isRepeat == 1)
+            {
+                isRepeat = 2;
+                RepeatButton.Image = Media_Player_DIY.Properties.Resources.BRepeat_all;
+            }
+            else if (isRepeat == 2)  
+            {
+                isRepeat = 0;
+                RepeatButton.Image = Media_Player_DIY.Properties.Resources.BRepeat_off;
+            }
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            TrackBarVolume.Value = 50;
+            mpUser.Volume = 0.5;
+        }
+
+        private void listBox1_DragDrop(object sender, DragEventArgs e)
+        {
+
+            foreach (string obj in (string[])e.Data.GetData(DataFormats.FileDrop))
+                if(Directory.Exists(obj))
+                    paths.AddRange(Directory.GetFiles(obj, "*.*", SearchOption.AllDirectories));
+                else
+                    paths.Add(obj);
+            for (int i = 0; i < paths.Count; i++)
+            {
+                listBox1.Items.Add(Path.GetFileNameWithoutExtension(paths[i]));
+            }
+        }
+
+        private void listBox1_DragEnter(object sender, DragEventArgs e)
+        {
+           if(e.Data.GetDataPresent(DataFormats.FileDrop))
+                e.Effect = DragDropEffects.Copy;
+        }
+
+        private void listBox1_SelectedIndexChanged_1(object sender, EventArgs e)
+        {
+            //listBox1.SelectedItem.ToString()
+            string item_music = paths[listBox1.SelectedIndex];
+            mpUser.Open(new Uri(item_music));
+            isMPopened = true;
+            mpUser.Play();
+            isMPplay = true;
+            //Thread.Sleep(100);
+            //StreamTimeSound();
+        }
+
+        void StreamTimeSound()
+        {
+            if (mpUser.NaturalDuration.HasTimeSpan)
+            {
+                TimeSound_double = Math.Round(mpUser.NaturalDuration.TimeSpan.TotalSeconds, 1);
+                label1.Text += "\n" + "Продолжительность трека: " + TimeSound_double.ToString();
+            }
+            
+        }
+
+        void OpenedSound(object sender, EventArgs e)
+        {
+            StreamTimeSound();
+            isMPopened=true;
+            NameTruckLabel.Text = Path.GetFileNameWithoutExtension(mpUser.Source.LocalPath);
+            TimelineTrackbar.Enabled = true;
+            //TimelineTrackbar_StreamTime();
+        }
+
+        void TimelineTrackbar_StreamTime()
+        {
+            while (true)
+            {
+                TimelineTrackbar.Value = (int)(mpUser.Position.TotalSeconds / TimeSound_double);
+                Thread.Sleep(1000);
+            }
+        }
+
+        void NextTrackPlay(object sender, EventArgs e)
+        {
+            if (isRepeat == 1)
+            {
+                string item_music = paths[listBox1.SelectedIndex];
+                mpUser.Open(new Uri(item_music));
+                mpUser.Play();
+                if(!isMPplay) isMPplay = true;
+            }
+            else if (isRepeat == 2 && listBox1.Items.Count > 1)
+            {
+                if (listBox1.SelectedIndex != listBox1.Items.Count - 1)
+                    listBox1.SelectedIndex = listBox1.SelectedIndex + 1;
+                else
+                    listBox1.SelectedIndex = 0;
+            }
+        }
+
+        void DebugLine()
+        {
+            //label1
+        }
+    }
+}
